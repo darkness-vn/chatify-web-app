e654d1bbcca36b4476fdb3c3bdde6cda5d4db6a9
Tung Hwang <rsoft.hwang99@gmail.com>
2023-09-06 21:36:55 +0700

    fix deploy 1

--- app/(main)/lobby/[zone]/page.tsx
+++ app/(main)/lobby/[zone]/page.tsx
@@ -1,28 +1,26 @@
-import { cookies } from "next/headers"
-import ZoneMenu from "./menu"
-import axios from "axios"
-import { redirect } from "next/navigation"
-import { iZone } from "@/types/zone"
+// import { cookies } from "next/headers"
+// import ZoneMenu from "./menu"
+// import axios from "axios"
+// import { redirect } from "next/navigation"
+// import { iZone } from "@/types/zone"
 
-export const getZoneDetail = async (zoneId: string) => {
-    try {
-        const cookieStore = cookies()
-        const token = cookieStore.get("token")?.value
-        const { data } = await axios.get(`http://localhost:8888/zone/zone-detail/${zoneId}`, { headers: { Authorization: token } })
-        return data
-    } catch (err: any) {
-        console.log(err.message)
-        return redirect("/login")
-    }
-}
+// export const getZoneDetail = async (zoneId: string) => {
+//     try {
+//         const cookieStore = cookies()
+//         const token = cookieStore.get("token")?.value
+//         const { data } = await axios.get(`http://localhost:8888/zone/zone-detail/${zoneId}`, { headers: { Authorization: token } })
+//         return data
+//     } catch (err: any) {
+//         console.log(err.message)
+//         return redirect("/login")
+//     }
+// }
 
-export default async function Zone ({params}: {params: {
-    zone: string
-}}) {
-
-    const data = await getZoneDetail(params.zone) as iZone
-
+export default async function Zone({ params }: {params: {zone: string}}) {
+    // const cookieStore = cookies()
+    // const token = cookieStore.get("token")?.value
+    // const { data } = await axios.get(`http://localhost:8888/zone/zone-detail/${params.zone}`, { headers: { Authorization: token } })
     return <div className="flex-grow bg-red-500">
-            <p>ZONE</p>
-        </div>
+        <p>ZONE</p>
+    </div>
 }
